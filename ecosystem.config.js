b7ac97d7bacd4764b1087e5c511a81d80d5293de
syropian <collinpatrickhenderson@gmail.com>
2020-06-28 14:50:03 -0400

    Updates nuxt prod port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,9 @@ module.exports = {
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
+      env: {
+        PORT: 6969,
+      },
     },
   ],
 }
