0163e44db561b7b09f7f27ee824c4a7ffe3a9d60
syropian <collinpatrickhenderson@gmail.com>
2020-10-18 14:33:56 -0400

    Updates pm2 config to increase node memory limit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,10 @@ module.exports = {
       name: 'Knot',
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start --port=6969',
+      node_args: '--max_old_space_size=4096',
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
   ],
 }
