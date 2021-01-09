0eab3318de02e450e5985d4381dfc211cce5b0ce
syropian <collinpatrickhenderson@gmail.com>
2021-01-09 10:05:33 -0500

    Pushes download button even farther down

--- components/posts/shared/PhotoSwipe.vue
+++ components/posts/shared/PhotoSwipe.vue
@@ -34,7 +34,7 @@
             download
             :href="imgSrc"
             target="_blank"
-            class="absolute z-50 inline-block w-6 h-6 right-4 top-8"
+            class="absolute z-50 inline-block w-6 h-6 right-4 top-10"
           >
             <DownloadIcon class="text-white" />
           </a>
