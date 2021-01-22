a452cc0428421664cacf0c9a249b18ed426e2fbf
syropian <collinpatrickhenderson@gmail.com>
2021-01-22 17:25:00 -0500

    Debugging stuff

--- layouts/default.vue
+++ layouts/default.vue
@@ -45,13 +45,33 @@ export default {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude: lat, longitude: long } = position.coords
-          if (!!lat && !!long) {
+          console.log(position.coords)
+          if (lat && long) {
             this.fetchCurrentLocation({ lat, long })
           }
         },
-        null,
+        (error) => {
+          switch (error.code) {
+            case error.PERMISSION_DENIED:
+              console.error('User denied the request for Geolocation.')
+              break
+
+            case error.POSITION_UNAVAILABLE:
+              console.error('Location information is unavailable.')
+              break
+
+            case error.TIMEOUT:
+              console.error('The request to get user location timed out.')
+              break
+
+            case error.UNKNOWN_ERROR:
+              console.error('An unknown error occurred.')
+              break
+          }
+        },
         {
           enableHighAccuracy: true,
+          maximumAge: 0,
         }
       )
     },
