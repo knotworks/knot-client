377e063ac881fe66c34aff3dc2fbb4c84b7024e5
syropian <collinpatrickhenderson@gmail.com>
2021-05-17 15:22:27 -0400

    Updates bugsnag config...again

--- nuxt.config.js
+++ nuxt.config.js
@@ -139,6 +139,11 @@ export default {
       'nuxt-bugsnag',
       {
         apiKey: process.env.BUGSNAG_API_KEY,
+        reporterOptions: {
+          releaseStage: process.env.NODE_ENV,
+          autoAssignRelease: true,
+        },
+        publishRelease: true,
       },
     ],
   ],
